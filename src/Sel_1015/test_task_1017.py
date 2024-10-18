f52226d58743d30ff6ab0b9b8178b830ead31b4e
sasiSDN <sasisdn861999@gmail.com>
2024-10-18 22:55:49 +0530

    17th files of october

--- src/Sel_1015/test_task_1017.py
+++ src/Sel_1015/test_task_1017.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+
 def test_login():
     driver=webdriver.Chrome()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
